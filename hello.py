24bba300e388132c82f37c78fb3e4c9171287f89
unasir335 <unasir@csu.fullerton.edu>
2022-08-29 09:56:15 -0700

    upload lab01 draft1

---
+++ hello.py
@@ -0,0 +1,10 @@
+# Name: Umar Nasir
+# Date: 08/28/2022
+# File Purpose: Multiple hello functions
+
+def helloworld():
+    return "Hello World!"
+
+
+def helloname(name):
+    return "Hello " + name + "!"
---
+++ main.py
@@ -0,0 +1,10 @@
+# Name: Umar Nasir
+# Date: 08/28/2022
+# File Purpose: Lab01 main program driver program
+
+from hello import *
+
+print("*** TUFFY TITAN HELLO PROGRAM ***")
+
+print(helloworld())
+print(helloname("Steve"))
